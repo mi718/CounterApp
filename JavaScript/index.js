6810149e919509a329c3a135c45431414d795477
micael <micael.staeubli@gmail.com>
2023-01-04 09:56:15 +0100

    feat: button functions assign

---
+++ JavaScript/index.js
@@ -0,0 +1,25 @@
+let countEl = document.getElementById("count-el")
+
+console.log(countEl)
+
+let count = 0
+
+function increment() {
+
+ count = count + 1
+ countEl.innerText = count
+ console.log(count)
+ 
+}
+
+function decrease(){
+ count = count -1
+ countEl.innerText = count
+
+}
+
+function save(){
+ let number = count
+ count = 0
+ countEl.innerText = count
+}
